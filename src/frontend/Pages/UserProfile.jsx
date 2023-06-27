e4bacd4cc386c0647f62f7bc2190b455b4fd9e84
Saket kumar <kumarsaket601@gmail.com>
2023-06-27 11:23:46 +0530

    modified: userProfile responsiveness

--- src/frontend/Pages/UserProfile.jsx
+++ src/frontend/Pages/UserProfile.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { PostCard } from "../services";
 import { getUser } from "../Slice/userSlice";
+import { getPosts } from "../Slice/postSlice";
 
 const UserProfile = () => {
   const { username } = useParams();
@@ -20,6 +21,7 @@ const UserProfile = () => {
       navigate("/login");
     } else {
       dispatch(getUser());
+      dispatch(getPosts());
     }
   }, [dispatch, token, navigate]);
 
@@ -31,7 +33,7 @@ const UserProfile = () => {
   );
 
   return (
-    <div className="mt-8">
+    <div className="my-8 mx-7">
       <section className="flex flex-col items-center justify-center  text-white ">
         <img
           src={currentProfile?.avatarURL}
@@ -43,18 +45,19 @@ const UserProfile = () => {
             {currentProfile?.firstName}
           </span>
           <span className="text-2xl font-bold">{currentProfile?.lastName}</span>
-          <button className="text-cyan-600 text-sm">Edit</button>
         </div>
         <div className="flex flex-col line-height-2">
-          <span className="text-slate-400 text-center">
+          <span className="text-center text-lg text-cyan-600 mt-4">
             @{currentProfile?.username}
           </span>
-          <p className="text-lg text-cyan-600 mt-4 ">{currentProfile?.bio}</p>
+          <p className="text-center text-lg text-cyan-600 mt-4">
+            {currentProfile?.bio}
+          </p>
           <Link className="hover:text-cyan-600 text-sm text-center">
             {currentProfile?.website}
           </Link>
         </div>
-        <div className=" flex gap-3 mt-4 text-xs">
+        <div className=" flex flex-wrap gap-3 mt-4 text-xs ">
           <button className=" flex-1 rounded-3xl border-cyan-600 border-2 px-3 py-2">
             post<span className="ml-1">{currentProfilePost.length}</span>
           </button>
@@ -68,10 +71,12 @@ const UserProfile = () => {
           </button>
         </div>
       </section>
-      <section className="my-10">
-        <div className=" max-w-lg w-full m-auto">
+      <section className="my-10 md:col-start-2 md:col-end-6 lg:col-start-2 lg:col-end-5">
+        <div className=" max-w-lg w-full m-auto my-10 md:col-start-2 md:col-end-6 lg:col-start-2 lg:col-end-5">
           {currentProfilePost.length === 0 ? (
-            "There no post that users has."
+            <p className="text-white text-center">
+              There is no post that user has.
+            </p>
           ) : (
             <div className="flex flex-col mt-6 text-white gap-2">
               {currentProfilePost?.reverse().map((post) => (
